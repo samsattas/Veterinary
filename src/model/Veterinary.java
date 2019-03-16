8833ad8a1b027bef63a91d7be95bd8ebb4c8ccc6
SAMUEL SATIZABAL <samuelsatizabal@hotmail.com>
2019-03-15 20:36:52 -0500

    actualizacion de la clase veterinary y la clase Client

--- src/model/Veterinary.java
+++ src/model/Veterinary.java
@@ -1,3 +1,21 @@
+package model;
+
 public class Veterinary{
-	
+	private String name;
+	private Client[] nClient;
+
+
+	//constructor
+	public Veterinary(String name, Client[] nClient){
+		this.name = name;
+		this.nClient = nClient;
+
+	}
+
+
+	public String getName(){return name;}
+	public Client[] nClient(){return nClient;}
+
+	public void setName(String name){this.name = name;}
+	public void setNClient(Client[] nClient){this.nClient = nClient;}
 }
